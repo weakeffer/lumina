ab5c29d03c3bb12758571670905ece320324fb81
weakeffer <hstqwl1p@gmail.com>
2026-03-20 23:27:12 +0300

    chore(frontend): update configuration and entry point

--- frontend/lumina-front/vite.config.js
+++ frontend/lumina-front/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    }
+  }
 })
